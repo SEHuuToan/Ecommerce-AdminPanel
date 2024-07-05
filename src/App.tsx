e8ae72d2d2f0ff5a1461f27b473051313f375c53
huutoanrobotic <huutoanit2023@gmail.com>
2024-07-05 14:18:57 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import Navbar from "./components/navbar/Navbar";
 import Admin from "./pages/admin/Admin";
 const App = () => {
   return(
-    <div>
+    <>
       <Navbar />
       <Admin />
-    </div>
+    </>
   );
 }
 
